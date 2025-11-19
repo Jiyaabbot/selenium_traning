f27c94b4d2550827aa31b21858f823ae33e68e0a
Jiyaabbot <jiyaabbot@gmail.com>
2025-11-19 18:58:59 +0530

    adding for loop

--- learning.py
+++ learning.py
@@ -1,4 +1,21 @@
+class number:
+
+    def even_odd(self, start, end):
+        for ele in range(start, end):
+            if ele%2 ==0:
+                print("even")
+            else:
+                print("odd")
+
+num = number()
+num.even_odd(12,20)
+
+
+
+
+
 if 10%5==0:
     print("even")
 else:
     print("odd")
+
